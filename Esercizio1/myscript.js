a436690ba98e1eb831e937629242c2bf1d10f340
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-05-22 16:52:50 +0200

    modifica nel codice

--- Esercizio1/myscript.js
+++ Esercizio1/myscript.js
@@ -1,19 +1,31 @@
 const UserWord = prompt('Type a word');
 
+let array = [];
+
 if (isWordPalindrom(UserWord)){
-    console.log('la parola è palindroma');
+    console.log(' la parola è palindroma');
 } else {
-    console.log('la parola non è palindroma');
+    console.log(' la parola non è palindroma');
 }
 
 function isWordPalindrom(word){
     for (let i = 0; i < (word.length / 2 ) + 1 ; i = i + 1){
         if ( word[i] === word[word.length - i]){
-            console.log(' la lettera numero' + i + ' ' + 'è uguale alla lettera numero' + (word.length - i)); 
-             return true;
+            console.log(' la lettera numero ' + i + ' ' + 'è uguale alla lettera numero ' + ' ' + (word.length - i));
+            array.push(i);
+            console.log(array);
+            
         } else {
-            console.log(' la lettera numero' + i + ' ' + 'non è uguale alla lettera numero' + (word.length - i)); 
-             return false;        
+            console.log(' la lettera numero ' + i + ' ' + 'non è uguale alla lettera numero ' + (word.length - i)); 
+                 
         }
+    }
+    
+    if (array.length === word.length){
+        console.log('la parola è palindroma');
+        return true;
+    } else {
+        console.log('la parola non è palindroma');
+        return false;
     }
 }
