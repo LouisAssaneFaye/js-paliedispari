33fdd4f3535f655380e94cc67495fb3c76c05271
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-05-22 16:23:01 +0200

    correzione es 1

--- Esercizio1/myscript.js
+++ Esercizio1/myscript.js
@@ -9,10 +9,10 @@ if (isWordPalindrom(UserWord)){
 function isWordPalindrom(word){
     for (let i = 0; i < (word.length / 2 ) + 1 ; i = i + 1){
         if ( word[i] === word[word.length - i]){
-            console.log(' la lettera numero' + i + ' ' + 'è uguale alla lettera numero' + ([word.length - i)); 
+            console.log(' la lettera numero' + i + ' ' + 'è uguale alla lettera numero' + (word.length - i)); 
              return true;
         } else {
-            console.log(' la lettera numero' + i + ' ' + 'non è uguale alla lettera numero' + ([word.length - i)); 
+            console.log(' la lettera numero' + i + ' ' + 'non è uguale alla lettera numero' + (word.length - i)); 
              return false;        
         }
     }
