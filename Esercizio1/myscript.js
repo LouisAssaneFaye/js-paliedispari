130205323064c61d8a2101ac86db58403de1fd1a
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-05-22 18:17:20 +0200

    Esercizio 1 terminato

--- Esercizio1/myscript.js
+++ Esercizio1/myscript.js
@@ -4,29 +4,47 @@ let array = [];
 
 if (isWordPalindrom(UserWord)){
     console.log(' la parola è palindroma');
+    document.getElementById('prova').innerHTML=' la parola è palindroma'
 } else {
     console.log(' la parola non è palindroma');
+    document.getElementById('prova').innerHTML=' la parola non è palindroma'
 }
 
 function isWordPalindrom(word){
-    for (let i = 0; i < (word.length / 2 ) + 1 ; i = i + 1){
-        if ( word.charAt(i) === word.charAt(word.length - i)){
-            console.log(' la lettera numero ' + i + ' ' + 'è uguale alla lettera numero ' + ' ' + (word.length - i));
+    for (let i = 0; i < (word.length / 2 ) ; i = i + 1){
+        if ( word.charAt(i) === word.charAt(word.length - 1 - i)){
+            console.log(' la lettera numero ' + i + ' ' + 'è uguale alla lettera numero ' + ' ' + (word.length - 1 - i));
+            console.log(word.charAt(i), word.charAt(word.length - 1 - i)  );
             array.push(i);
             console.log(array);
             
         } else {
-            console.log(' la lettera numero ' + i + ' ' + 'non è uguale alla lettera numero ' + (word.length - i)); 
+            console.log(' la lettera numero ' + i + ' ' + 'non è uguale alla lettera numero ' + (word.length - 1 - i));
+            console.log(word.charAt(i), word.charAt(word.length - 1 - i) ); 
                  
         }
     }
+   
     
-    if (array.length === word.length){
-        console.log('la parola è palindroma');
-        return true;
+    if ( word.length % 2 === 0 ){
+        if (array.length === (word.length / 2)){
+            console.log('la parola è palindroma');
+            return true;
+        } else {
+            console.log('la parola non è palindroma');
+            return false;
+        }
     } else {
-        console.log('la parola non è palindroma');
-        return false;
+        if (array.length === (word.length / 2 - 0.5)){
+            console.log('la parola è palindroma');
+            return true;
+        } else {
+            console.log('la parola non è palindroma');
+            return false;
+        }
     }
-}
 
+
+
+
+}
